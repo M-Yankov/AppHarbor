ef81e965543ae3d92f2b8450498396e0fed93d76
M-Yankov <pointblank12345@abv.bg>
2015-11-16 02:33:09 +0200

    Application

---
+++ CloudServices.Common/GlobalConstatns.cs
@@ -0,0 +1,8 @@
+﻿namespace CloudServices.Common
+{
+    public class GlobalConstatns
+    {
+        public const string ProjectID = "5648eff19195a800070000a1";
+        public const string Token = "V3FndYg1KqHgV67cV6q7";
+    }
+}
